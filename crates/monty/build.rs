8369815a683817f281b8b7207645bf38630211b0
David Hewitt <mail@davidhewitt.dev>
2026-02-20 11:32:45 +0000

    split `pyo3-build-config` change off to other PRs

--- crates/monty/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // This ensures that the tests and benchmarks link to exact path to libpython, which avoids need
-    // to manually set LD_LIBRARY_PATH when running them.
-    pyo3_build_config::add_libpython_rpath_link_args();
-}
